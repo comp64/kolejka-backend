03205c92723992e5856768266e1b834d7d91093f
Pavol Hronsky <pavol.hronsky@nordea.com>
2017-03-10 13:46:58 +0100

    delivery car implementation and test

---
+++ src/main/java/com/tamara/kolejka/DeliveryCar.java
@@ -0,0 +1,27 @@
+package com.tamara.kolejka;
+
+import com.tamara.kolejka.model.cards.product.ProductCard;
+
+import java.util.List;
+
+public class DeliveryCar {
+
+    private List<ProductCard> stock;
+
+    public DeliveryCar(List<ProductCard> stock) {
+        this.stock = stock;
+    }
+
+    public ProductCard takeTopProduct() {
+        return stock.remove(stock.size()-1);
+    }
+
+    public void addProduct(ProductCard productCard) {
+        stock.add(productCard);
+    }
+
+    public int size() {
+        return stock.size();
+    }
+
+}
---
+++ src/test/java/com/tamara/kolejka/DeliveryCarTest.java
@@ -0,0 +1,73 @@
+package com.tamara.kolejka;
+
+import com.tamara.kolejka.model.cards.product.ProductCard;
+import com.tamara.kolejka.model.enums.ProductType;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class DeliveryCarTest {
+
+    private final Long ID = Long.valueOf(1L);
+    private final ProductType PRODUCT_TYPE = ProductType.APPLIANCE;
+    private final String TITLE = "title";
+    private final String DESCRIPTION = "description";
+
+    @Rule
+    public TestName mName = new TestName();
+
+    private DeliveryCar deliveryCar;
+
+    @Before
+    public void setUp() {
+        List<ProductCard> cards = new ArrayList<>();
+        ProductCard applianceCard = new ProductCard();
+        applianceCard.setId(ID);
+        applianceCard.setProductType(PRODUCT_TYPE);
+        applianceCard.setTitle(TITLE);
+        applianceCard.setDescription(DESCRIPTION);
+        cards.add(applianceCard);
+
+        deliveryCar = new DeliveryCar(cards);
+    }
+
+    @Test
+    public void takeTopProduct() throws Exception {
+        log.info("{}: Start", mName.getMethodName());
+        ProductCard removedCard = deliveryCar.takeTopProduct();
+        Assert.assertNotNull(removedCard);
+        Assert.assertEquals(ID, removedCard.getId());
+        Assert.assertEquals(PRODUCT_TYPE, removedCard.getProductType());
+        Assert.assertEquals(TITLE, removedCard.getTitle());
+        Assert.assertEquals(DESCRIPTION, removedCard.getDescription());
+        Assert.assertEquals(0, deliveryCar.size());
+        log.info("{}: Finished", mName.getMethodName());
+    }
+
+    @Test
+    public void addProduct() throws Exception {
+        log.info("{}: Start", mName.getMethodName());
+        ProductCard applianceCard = new ProductCard();
+        applianceCard.setId(ID);
+        applianceCard.setProductType(PRODUCT_TYPE);
+        applianceCard.setTitle(TITLE);
+        applianceCard.setDescription(DESCRIPTION);
+        deliveryCar.addProduct(applianceCard);
+        Assert.assertEquals(2, deliveryCar.size());
+        log.info("{}: Finished", mName.getMethodName());
+    }
+
+    @Test
+    public void size() throws Exception {
+        log.info("{}: Start", mName.getMethodName());
+        Assert.assertEquals(1, deliveryCar.size());
+        log.info("{}: Finished", mName.getMethodName());
+    }
+}
